5ac15dcb8da8ce928f618597d64909f15d51fdee
prasathnarayanan6 <prasathnarayanan6@gmail.com>
2025-01-08 12:22:20 +0530

    Connection changes

--- utils/conn.js
+++ utils/conn.js
@@ -4,14 +4,14 @@ dotenv.config();
 const client = new Client({
   host: "localhost",
   user: "postgres",
-  port: "3306",
+  port: "3306" || "5432",
   password: "1234",
   database: "postgres"
 })
 client.connect(function(err) {
   if(err) throw err;
   else{
-    console.log("connected");
+    console.log("connected test");
   } 
 });
 module.exports = client;
