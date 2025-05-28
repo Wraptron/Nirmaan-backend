25c9f44949da3c09272077cc49896a62193ce857
prasathnarayanan6 <prasathnarayanan6@gmail.com>
2025-05-28 20:14:31 +0530

    push

--- model/AwsModel.js
+++ model/AwsModel.js
@@ -1,32 +1,33 @@
 const aws = require('aws-sdk');
 const path = require('path')
-require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const multer = require('multer');
 const multers3 = require("multer-s3");
 const fs = require('fs');
 aws.config.update({
-    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
     accessKeyId:process.env.AWS_ACCESS_KEY,
     region:process.env.REGION
 })
-const s3= new aws.S3();
-const AwsModel = async(filename) => 
-{
+console.log(process.env.AWS_SECRET_KEY);
+//const s3= new aws.S3();
+// const AwsModel = async(filename) => 
+// {
 
-    const fileStream = fs.createReadStream(filename.path);
-    const uploadParams = {
-        Bucket: process.env.AWS_S3_BUCKET,
-        Key: `Mentor/${filename.originalname}`,
-        Body: fileStream,
-        //ACL: 'public-read',
-        ContentType: filename.mimetype,
-    };
-    try {
-        const result = await s3.upload(uploadParams).promise();
-        return result.Location;
-      } catch (error) {
-        console.error("Upload error:", error);
-        throw error;
-    }
-}
-module.exports = {AwsModel};
+//     const fileStream = fs.createReadStream(filename.path);
+//     const uploadParams = {
+//         Bucket: process.env.AWS_S3_BUCKET,
+//         Key: `Mentor/${filename.originalname}`,
+//         Body: fileStream,
+//         //ACL: 'public-read',
+//         ContentType: filename.mimetype,
+//     };
+//     try {
+//         const result = await s3.upload(uploadParams).promise();
+//         return result.Location;
+//       } catch (error) {
+//         console.error("Upload error:", error);
+//         throw error;
+//     }
+// }
+// module.exports = {AwsModel};
