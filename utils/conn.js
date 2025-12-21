748cf49f208857c3f5b1729a38354f44da0cb3ac
Prasath Narayanan <prasathnarayanan6@gmail.com>
2025-12-21 21:44:32 +0530

    Pushing if all things work good

--- utils/conn.js
+++ utils/conn.js
@@ -17,3 +17,17 @@
 // });
 // module.exports = client;
 // db.js
+const { Client } = require("pg");
+
+//const connectionString = "postgres://postgres:Supabase%401234@db.rcoyvhpccbezoprhqhwy.supabase.co:5432/postgres";
+const connectionString = "postgresql://postgres.rcoyvhpccbezoprhqhwy:Supabase%401234@aws-1-ap-south-1.pooler.supabase.com:5432/postgres";
+const client = new Client({
+  connectionString,
+});
+
+client.connect((err) => {
+  if (err) console.error("Connection error:", err.stack);
+  else console.log("Connected successfully!!");
+});
+
+module.exports = client;
